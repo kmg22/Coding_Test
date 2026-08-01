afab55b1bb06b929ef390b021bb0dcdff32b6510
kmg22 <ssukmg22@gmail.com>
2026-08-01 23:43:46 +0900

    [lv.2] #43165 타겟넘버 | dfs, 조합

---
+++ @Combinatorics/programmers_43165/README.md
@@ -0,0 +1,5 @@
+[[lv.2] #43165 타겟넘버](https://school.programmers.co.kr/learn/courses/30/lessons/43165)
+---
+
+- 조합
+- dfs
---
+++ @Combinatorics/programmers_43165/src/App.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    private static int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+    
+    private static void dfs(int[] numbers, int target, int index, int sum){
+        if(index==numbers.length){
+            if(sum==target){
+                answer++;
+            }
+            return;
+        }
+        dfs(numbers, target, index+1, sum+numbers[index]);
+        dfs(numbers, target, index+1, sum-numbers[index]);
+    }
+    
+}
